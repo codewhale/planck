19ebf62c164f6911add576b1083c3a63fb9bec20
Mike Fikes <mike@fikesfarm.com>
2018-04-17 09:28:18 -0400

    Diagnostics surrounding tasks

--- planck-c/tasks.c
+++ planck-c/tasks.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <stdio.h>
 #include "tasks.h"
 
 static int tasks_outstanding = 0;
@@ -10,7 +11,9 @@ int block_until_tasks_complete() {
     if (err) return err;
 
     while (tasks_outstanding) {
+        fprintf(stderr, "block_until_tasks_complete wait %d\n", tasks_outstanding);
         err = pthread_cond_wait(&tasks_complete_cond, &tasks_complete_lock);
+        fprintf(stderr, "block_until_tasks_complete wait done %d %d\n", err, tasks_outstanding);
         if (err) {
             pthread_mutex_unlock(&tasks_complete_lock);
             return err;
@@ -36,9 +39,11 @@ int signal_task_started() {
 }
 
 int signal_task_complete() {
+    fprintf(stderr, "signal_task_complete\n");
     int err = pthread_mutex_lock(&tasks_complete_lock);
     if (err) return err;
 
+    fprintf(stderr, "signal_task_complete %d\n", tasks_outstanding);
     tasks_outstanding--;
 
     err = pthread_cond_signal(&tasks_complete_cond);
